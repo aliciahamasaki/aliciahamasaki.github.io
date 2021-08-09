0aa11170b42986a0e97fe52d145d342b00cb80e8
A J <ajscarfe30@gmail.com>
2021-08-09 19:36:47 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Pink Box</h1>
+        <p>
+          This is a paragraph.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
